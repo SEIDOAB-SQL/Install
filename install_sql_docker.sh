45c341dbdf6e01585c56f16185364fc751ce0374
Martin <martin_lenart@icloud.com>
2025-04-21 16:23:49 +0200

    initial commit

---
+++ install_sql_docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#SQL SERVER 2022
+#pull the container image to my computer
+docker pull mcr.microsoft.com/mssql/server:2022-latest
+
+#install and run the container 
+docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=skYhgS@83#aQ" -p 14333:1433 --name sql2022container --hostname sql2022 -d mcr.microsoft.com/mssql/server:2022-latest
