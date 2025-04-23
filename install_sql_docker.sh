956a9570e7905081e0c1d3b2c89ee2c0e3f2e65f
Martin <martin_lenart@icloud.com>
2025-04-23 13:24:16 +0200

    update

--- install_sql_docker.sh
+++ install_sql_docker.sh
@@ -6,3 +6,6 @@ docker pull mcr.microsoft.com/mssql/server:2022-latest
 
 #install and run the container 
 docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=skYhgS@83#aQ" -p 14333:1433 --name sql2022container --hostname sql2022 -d mcr.microsoft.com/mssql/server:2022-latest
+
+#connection string
+#Data Source=localhost,14333;Persist Security Info=True;User ID=sa;Pwd=skYhgS@83#aQ;Encrypt=False;
