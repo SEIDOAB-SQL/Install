3d892b22ae0a82c79447828af8bcd467c4f9d2e6
Martin <martin_lenart@icloud.com>
2025-04-24 10:14:33 +0200

    update

--- install_sql_docker.sh
+++ install_sql_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #SQL SERVER 2022
+################
 #pull the container image to my computer
 docker pull mcr.microsoft.com/mssql/server:2022-latest
 
@@ -9,3 +10,27 @@ docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=skYhgS@83#aQ" -p 14333:1433 
 
 #connection string
 #Data Source=localhost,14333;Persist Security Info=True;User ID=sa;Pwd=skYhgS@83#aQ;Encrypt=False;
+
+
+#MariaDb
+########
+#pull the container image to my computer
+docker pull mariadb
+
+# create a database container and run the docker 
+docker run --name mariadbcontainer -e MYSQL_ROOT_PASSWORD=skYhgS@83#aQ -p 3306:3306 -d mariadb
+
+#connection string
+#server=localhost;uid=root;pwd=skYhgS@83#aQ
+
+
+#Postgresql
+##########
+#pull the container image to my computer
+docker pull postgres
+
+# create a database container and run the docker 
+docker run --name postgrescontainer -e POSTGRES_PASSWORD=skYhgS@83#aQ -d -p 5432:5432 postgres
+
+#connection string
+#Server=localhost;Port=5432;User Id=postgres;Password=skYhgS@83#aQ;
